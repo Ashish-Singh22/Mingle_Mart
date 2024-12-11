64166bc389ce821ab6b731646fcecff68e95b357
Ashish kumar Singh <139993841+Ashish-Singh22@users.noreply.github.com>
2024-12-11 14:27:46 +0530

    done

--- update_cart_value.js
+++
@@ -1,5 +0,0 @@
- const add_to_cart_item_no=document.querySelector(".add_to_cart_item_no");
-
-export const update_cart_value=(local_storage_array)=>{
-    return (add_to_cart_item_no.textContent=local_storage_array.length);
-};
